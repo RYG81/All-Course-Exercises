95191cd7a476c61ae31443e3387ae7f4f5d56017
Rahul Gupta <rahul@Rahuls-Mac-mini.local>
2015-11-07 18:13:09 +0530

    Added Lecture 22 Exrcise project - Function

---
+++ Lecture22-Exercise-Function/Lecture22-Exercise-Function/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture22-Exercise-Function
+//
+//  Created by Rahul Gupta on 07/11/15.
+//  Copyright © 2015 Rahul gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
